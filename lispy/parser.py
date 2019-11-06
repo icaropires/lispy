b70408c5b4192d9a7d538ca17e9fcae277d11af0
joaorobson <joaorobsonmartins@gmail.com>
2019-11-06 18:49:05 -0300

    Add grammar and transformer methods

--- lispy/parser.py
+++ lispy/parser.py
@@ -1,12 +1,27 @@
 from lark import Lark, InlineTransformer
 from pathlib import Path
+import re
 
 from .runtime import Symbol
 
 
 class LispTransformer(InlineTransformer):
+    number = float 
+
+    def string(self,  string):
+        return str(string[1:-1]).replace('\\n', '\n').replace('\\t', '\t').replace('\\f', '\f').replace('\\r', '\r').replace('\\"', '\"')
+
+    def op(self, op):
+        return Symbol(op)
+
+    def name(self, name):
+        return Symbol(name)
+
     def start(self, *args): 
         return [Symbol.BEGIN, *args]
+
+    def bool(self, value):
+        return True if value == "#t" else False
 
 def parse(src: str):
     """
